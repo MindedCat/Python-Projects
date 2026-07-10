3c19832d0f16e5a5108d31f6569c541bf58694ea
MindedCat <mindedtomcat@gmail.com>
2026-07-10 22:27:53 +0600

    day 1: added basic todo input and output. Did bonus task 1.

---
+++ Todo List App/bonus1.py
@@ -0,0 +1,3 @@
+title = input("Enter title: ")
+title_length = len(title)
+print("Title length:", title_length)
---
+++ Todo List App/main.py
@@ -0,0 +1,7 @@
+user_prompt = "Enter todo:"
+user_input1 = input(user_prompt)
+user_input2 = input(user_prompt)
+user_input3 = input(user_prompt)
+
+todos = [user_input1, user_input2, user_input3]
+print("Your todo list:", todos)
